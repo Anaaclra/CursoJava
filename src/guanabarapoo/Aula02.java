57bd6dc8e1b7ffede1f59b001cd37f4745e0250e
Ana Clara <anaclaraa02pereira@gmail.com>
2025-08-07 09:00:08 -0300

    Exercicios para aprendizado

--- src/guanabarapoo/Aula02.java
+++ src/guanabarapoo/Aula02.java
@@ -1,18 +1,18 @@
-package guanabarapoo;
-
-public class Aula02 {
-
-	public static void main(String[] args) {
-		Caneta c1 = new Caneta();
-		c1.modelo = "BIC Cristal";
-		c1.cor = "Azul";
-		//c1.ponta = "0.5";
-		c1.carga = 80;
-		c1.destampar();
-		c1.status();
-		c1.rabiscar();
-		
-		
-	}
-
-}
+//package guanabarapoo;
+//
+//public class Aula02 {
+//
+//	public static void main(String[] args) {
+//		Caneta02 c1 = new Caneta02();
+//		c1.modelo = "BIC Cristal";
+//		c1.cor = "Azul";
+//		//c1.ponta = "0.5";
+//		c1.carga = 80;
+//		c1.destampar();
+//		c1.status();
+//		c1.rabiscar();
+//		
+//		
+//	}
+//
+//}
