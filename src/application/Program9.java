f7ac41a7d953175fe505b665ec63094a00ce7d5b
Ana Clara <anaclaraa02pereira@gmail.com>
2025-08-17 21:32:17 -0300

    Exercicios para aprendizado

---
+++ src/application/Program9.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program9 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of Tax payer: ");
+		int n = sc.nextInt();
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
